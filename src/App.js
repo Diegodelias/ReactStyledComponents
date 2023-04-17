de6966c9c7dd3d7a182ce7b29dc5bacd81422c76
Diegodelias <diegodelias@gmail.com>
2023-04-17 18:19:35 -0300

    clases 3 y 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
 import Header from "./Components/Header";
-import Container from "./Components/Container";
+import Container from "./Components/Container/Index";
 import GlobalStyle from "./GlobalStyle"
+import { temaClaro , temaOscuro } from "./Components/UI/temas";
 
+
+console.log(temaClaro , temaOscuro)
 function App() {
   return (
-    <>
+    <ThemeProvider theme={ temaOscuro }>
      <GlobalStyle/>
       <Header />
       <Container />
-    </>
+    </ThemeProvider>
+    
   );
 }
 
