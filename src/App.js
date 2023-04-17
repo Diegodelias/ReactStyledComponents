c3a9be1d43e4dddc8c7582a50c14ee60db00279a
Diegodelias <diegodelias@gmail.com>
2023-04-17 19:28:17 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { ThemeProvider } from "styled-components";
 
 import Header from "./Components/Header";
 import Container from "./Components/Container/Index";
 import GlobalStyle from "./GlobalStyle"
 import { temaClaro , temaOscuro } from "./Components/UI/temas";
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from "./Components/SwitcherTema";
 
 
 console.log(temaClaro , temaOscuro)
 function App() {
+  const [tema,setTema] = useState(true)
+  const toggleTema = () => {
+    setTema((tema) => !tema)
+  }
+  
   return (
-    <ThemeProvider theme={ temaOscuro }>
+    <ThemeProvider theme={tema ? temaClaro : temaOscuro }>
      <GlobalStyle/>
+     <BtnTema onClick={toggleTema} >
+      <SwitcherTema tema={tema}/>
+
+     </BtnTema>
       <Header />
       <Container />
     </ThemeProvider>
--- src/Components/UI/index.js
+++ src/Components/UI/index.js
@@ -1,11 +1,13 @@
 
-
-
 import styled from "styled-components";
 
 export const Icono = styled.img`
   height: 25px;
   width: 25px;
+`;
+
+export const IconoTema = styled(Icono)`
+  filter: ${({ theme }) => theme.filter};
 `;
 
 export const Btn = styled.button`
@@ -25,7 +27,8 @@ export const Box = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  background-color: white;
+  background-color: ${({ theme }) => theme.inside};
+  color: ${({ theme }) => theme.text};
   border-radius: 5px;
   box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.04);
   padding: 20px;
@@ -44,4 +47,13 @@ export const Saldo = styled.div`
 export const Detalle = styled.span`
   color: #41d3be;
   font-size: 24px;
+`;
+
+export const BtnTema = styled.div`
+  position: absolute;
+  top: 4vh;
+  right: 20px;
+  background-color: inherit;
+  border: none;
+  cursor: pointer;
 `;
